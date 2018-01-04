ac0de137c9daaa9a7fa753865df84bd8c287a329
joeyzhao <joeyzhao1005@gmail.com>
2018-01-04 10:14:33 +0800

    升级到java8

--- src/main/java/com/kit/receiver/bootreceiver/BootReceiver.java
+++ src/main/java/com/kit/receiver/bootreceiver/BootReceiver.java
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.kit.app.core.task.DoSomeThing;
+import com.kit.utils.AppUtils;
+
 public class BootReceiver extends BroadcastReceiver implements IBootService {
 
 	public Context mContext;
@@ -12,7 +15,6 @@ public class BootReceiver extends BroadcastReceiver implements IBootService {
 	public void onReceive(Context context, Intent intent) {
 		mContext = context;
 		startBootService();
-
 	}
 
 	@Override
