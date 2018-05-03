e1a73c5c7d26459216b8e09d418aa7e6ca44c74a
joeyzhao <joeyzhao1005@gmail.com>
2018-05-03 19:46:24 +0800

    color

--- src/main/java/com/kit/ui/base/BaseAppCompatActivity.java
+++ src/main/java/com/kit/ui/base/BaseAppCompatActivity.java
@@ -2,9 +2,19 @@ package com.kit.ui.base;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.kit.app.ActivityManager;
 import com.kit.app.UIHandler;
@@ -12,15 +22,15 @@ import com.kit.utils.intent.IntentManager;
 import com.kit.utils.log.Zog;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
-public class BaseAppCompatActivity extends RxAppCompatActivity implements BaseV4Fragment.OnFragmentInteractionListener {
+public abstract class BaseAppCompatActivity extends RxAppCompatActivity implements BaseV4Fragment.OnFragmentInteractionListener {
 
-
-    private boolean isShowing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initWindow();
+        setContentView(layoutResID());
+        this.views = new SparseArray<>();
         getExtra();
         initWidget();
         loadData();
@@ -31,16 +41,85 @@ public class BaseAppCompatActivity extends RxAppCompatActivity implements BaseV4
         ActivityManager.getInstance().pushActivity(this);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    public boolean isShowing() {
+        return !isFinishing();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        isShowing = true;
+    /**
+     * 去网络或者本地加载数据
+     */
+    protected boolean loadData() {
+        return true;
     }
+
+
+    protected void initWidgetWithExtra() {
+    }
+
+    /**
+     * i
+     * 获得上一个Activity传过来的值
+     */
+    protected boolean getExtra() {
+
+        return true;
+    }
+
+    /**
+     * 初始化界面
+     */
+    protected void initWindow() {
+    }
+
+    /**
+     * 初始化界面
+     */
+    protected void initWidget() {
+    }
+
+    protected abstract int layoutResID();
+
+
+    public TextView getTextView(@IdRes int viewId) {
+        return getView(viewId);
+    }
+
+    public ImageView getImageView(@IdRes int viewId) {
+        return getView(viewId);
+    }
+
+    public ImageButton getImageButton(@IdRes int viewId) {
+        return getView(viewId);
+    }
+
+    public Button getButton(@IdRes int viewId) {
+        return getView(viewId);
+    }
+
+    public EditText getEditText(@IdRes int viewId) {
+        return getView(viewId);
+    }
+
+    public RatingBar getRatingBar(@IdRes int viewId) {
+        return getView(viewId);
+    }
+
+    public ProgressBar getProgressBar(@IdRes int viewId) {
+        return getView(viewId);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends View> T getView(@IdRes int viewId) {
+        View view = views.get(viewId);
+        if (view == null) {
+            view = findViewById(viewId);
+            views.put(viewId, view);
+        }
+        return (T) view;
+    }
+
+    private SparseArray<View> views;
+
 
     @Override
     protected void onStop() {
@@ -50,35 +129,6 @@ public class BaseAppCompatActivity extends RxAppCompatActivity implements BaseV4
         }
     }
 
-    public boolean isShowing() {
-        return isShowing;
-    }
-
-    /**
-     * 获得上一个Activity传过来的值
-     */
-    public boolean getExtra() {
-
-        return true;
-    }
-
-    /**
-     * 初始化界面
-     */
-    public void initWindow() {
-    }
-
-    /**
-     * 初始化界面
-     */
-    public void initWidget() {
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        isShowing = false;
-    }
 
     @Override
     protected void onDestroy() {
@@ -91,21 +141,11 @@ public class BaseAppCompatActivity extends RxAppCompatActivity implements BaseV4
 
 
     public void destory() {
+        views.clear();
         ActivityManager.getInstance().popActivity(this);
         IntentManager.get().destory(this);
-        isShowing = false;
     }
 
-    /**
-     * 去网络或者本地加载数据
-     */
-    public boolean loadData() {
-        return true;
-    }
-
-
-    public void initWidgetWithExtra() {
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -124,6 +164,7 @@ public class BaseAppCompatActivity extends RxAppCompatActivity implements BaseV4
 //        return super.onOptionsItemSelected(item);
 //    }
 //
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
