31a13b3a72845ef88ac0bd699e6252f2fc8045e7
joeyzhao <joeyzhao1005@gmail.com>
2019-11-25 11:16:47 +0800

    [feature] shortcuts补全

--- src/main/java/com/kit/shortcut/ZShortcutInfo.java
+++ src/main/java/com/kit/shortcut/ZShortcutInfo.java
@@ -367,7 +367,7 @@ public class ZShortcutInfo implements Cloneable {
         }
         int size = iconDrawableSize == 0 ? 32 : iconDrawableSize;
         PathDrawable pathDrawable = new PathDrawable(iconDrawablePathData, color, size, size);
-        Bitmap bitmap = DrawableUtils.drawableToBitmap(pathDrawable, size, size);
+        Bitmap bitmap = DrawableUtils.drawableToBitmap(pathDrawable, DensityUtils.dip2px(20), DensityUtils.dip2px(20));
         iconDrawable = new BitmapDrawable(bitmap);
     }
 
