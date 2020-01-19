908ebe6ca20f47c319ceb2e03c0447f6cfd855e0
joeyzhao <joeyzhao1005@gmail.com>
2020-01-19 19:59:20 +0800

    [feature] 负一屏添加微件 1

--- src/main/java/com/kit/ui/base/LifecycleKotlinCoroutineFragment.kt
+++ src/main/java/com/kit/ui/base/LifecycleKotlinCoroutineFragment.kt
@@ -33,4 +33,43 @@ open class LifecycleKotlinCoroutineFragment : Fragment() {
     }
 
 
+    /***
+     * 点击事件的View扩展
+     * @param block: (T) -> Unit 函数
+     * @return Unit
+     */
+    fun <T : View> T.click(block: (T) -> Unit) = setOnClickListener {
+        block(it as T)
+    }
+
+    /***
+     * 带延迟过滤的点击事件View扩展
+     * @param delay Long 延迟时间，默认600毫秒
+     * @param block: (T) -> Unit 函数
+     * @return Unit
+     */
+    fun <T : View> T.clickWithQuickCheck(time: Int = 600, block: (T) -> Unit) {
+        triggerDelay = time
+        setOnClickListener {
+            if (clickEnable()) {
+                block(it as T)
+            }
+        }
+    }
+
+
+    private fun <T : View> T.clickEnable(): Boolean {
+        var flag = false
+        val currentClickTime = System.currentTimeMillis()
+        if (currentClickTime - triggerLastTime >= triggerDelay) {
+            flag = true
+        }
+        triggerLastTime = currentClickTime
+        return flag
+    }
+
+    private var triggerDelay: Int = 600
+
+    private var triggerLastTime: Long = 0L
+
 }
