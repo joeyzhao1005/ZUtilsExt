73e76287550ef973347f35c61c11893659457b96
joeyzhao <joeyzhao1005@gmail.com>
2020-04-20 19:50:12 +0800

    [feature] reader launcher 2

--- src/main/java/com/kit/ui/base/BaseActivity.java
+++ src/main/java/com/kit/ui/base/BaseActivity.java
@@ -1,18 +1,21 @@
 package com.kit.ui.base;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.AndroidRuntimeException;
 
 import com.kit.app.resouce.DrawableId;
 import com.kit.utils.ActionBarUtils;
 import com.kit.utils.ResWrapper;
 import com.kit.utils.ResourceUtils;
+import com.kit.utils.log.Zog;
 
 import kotlinx.coroutines.CoroutineScope;
 
 /**
  * @author joeyzhao
  */
-public abstract class BaseActivity extends BaseAppCompatActivity  implements BaseV4Fragment.OnFragmentInteractionListener{
+public abstract class BaseActivity extends BaseAppCompatActivity implements BaseV4Fragment.OnFragmentInteractionListener {
 
 
     @Override
@@ -30,6 +33,13 @@ public abstract class BaseActivity extends BaseAppCompatActivity  implements Bas
         ActionBarUtils.setHomeActionBar(this, ResourceUtils.getDrawableId(this, DrawableId.ic_back));
     }
 
-
+    @Override
+    public void startActivity(Intent intent) {
+        try {
+            super.startActivity(intent);
+        } catch (AndroidRuntimeException e) {
+            Zog.showException(e);
+        }
+    }
 }
 
